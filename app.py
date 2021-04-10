c1818c7555beeaaf7ba57ee3e3cbc56a533b4215
mandadhanush <76746419+mandadhanush@users.noreply.github.com>
2021-04-10 16:27:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,9 +97,9 @@ def upload():
     return None
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
     
     
-#if __name__ =="__main__":
-#   app.run(host='0.0.0.0',port=8080)
+if __name__ =="__main__":
+   app.run(host='0.0.0.0',port=8080)
